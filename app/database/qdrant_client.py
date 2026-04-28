9acc985d7ca4e093e8ab3aefcb5c2d1e34573998
Alif Rahmadian <alifrah78@yahoo.com>
2026-04-28 18:36:19 +0700

    feat(qdrant-setup): complete qdrant setup functions

--- app/database/qdrant_client.py
+++ app/database/qdrant_client.py
@@ -2,7 +2,6 @@ from app.utils import(
     QDRANT_API_KEY,
     QDRANT_URL,
     QDRANT_COLLECTION_NAME,
-    load_llm,
     load_embedder,
     logger
 )
@@ -12,10 +11,40 @@ from qdrant_client.http.models import VectorParams, Distance
 
 from langchain_qdrant import QdrantVectorStore
 
-def get_qdrant_client():
-    client = QdrantClient(
-        url=QDRANT_URL,
-        api_key=QDRANT_API_KEY
-    )
+
+client = QdrantClient(
+    url=QDRANT_URL,
+    api_key=QDRANT_API_KEY
+)
     
-    return client
+def create_collection():
+    try:
+        if not client.collection_exists(QDRANT_COLLECTION_NAME):
+            logger.info("Creating qdrant collection...")
+            client.create_collection(
+                    collection_name = QDRANT_COLLECTION_NAME,
+                    vectors_config = VectorParams(size=1536, distance = Distance.COSINE)
+                )
+        else:
+            logger.info("Collection already exist, skipping...")
+    except Exception as e:
+        logger.error(f"Error while creating qdrant collection: {e}")
+        raise
+
+embedder = load_embedder()
+
+def get_vector_store():
+    return QdrantVectorStore(
+        client=client,
+        collection_name=QDRANT_COLLECTION_NAME,
+        embedding=embedder
+    )
+
+def insert_documents(documents):
+    try:
+        logger.info("Inserting documents to qdrant database...")
+        vector_store = get_vector_store()
+        vector_store.add_documents(documents)
+    except Exception as e:
+        logger.error(f"Error while inserting documents: {e}")
+        raise
